63e78b5aea378452200567453c7a1cb1f8479357
Annahariprasad <70802617+Annahariprasad@users.noreply.github.com>
2021-02-21 23:08:53 +0530

    Update Rough.py

--- Rough.py
+++ Rough.py
@@ -61,9 +61,10 @@ def popElem(l):
     return l
 
 def sortElem(l):
-    for i in range(count(l)-1):
-        if l[i]>l[i+1]:
-            (l[i],l[i+1]) = (l[i+1],l[i])
+    for i in range(count(l)):
+        for j in range(i+1,count(l)):
+            if(l[i]>l[j]):
+                (l[i],l[j]) = (l[j],l[i])
     return l
 
 def bigElem(l):
